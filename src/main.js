7e5cf3032ff62b79da309285cce02260237dc003
huangtubiao <1334615045@qq.com>
2017-02-13 19:33:57 +0800

    fix cannot read property installed of undefined

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import VuexRouterSync from 'vuex-router-sync'
+import VuexRouterSync from 'vuex-router-sync'
 
 import App from './App'
-// import store from './vuex'
+import store from './store'
 import router from './router'
 
-// VuexRouterSync.sync(store, router)
+VuexRouterSync.sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
-    // store,
+    store,
     template: '<App/>',
     components: { App }
 })
